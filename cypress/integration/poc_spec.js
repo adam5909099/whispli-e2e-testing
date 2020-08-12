3fd06f32a4c6ee6b4e4ac4f9ed9d412a474e136e
adam5909099 <adam5909099@gmail.com>
2020-08-12 13:19:12 -0400

    Write test scenarios

--- cypress/integration/poc_spec.js
+++ cypress/integration/poc_spec.js
@@ -1,14 +1,84 @@
-describe('proof of concept', () => {
-  it ('login', () => {
-    // Visit login page
-    cy.visit('/inbox/login');
+it('Proof of Concept', () => {
+  // Login
+  cy.visit('/inbox/login');
 
-    // Enter username and password
-    cy.get('name=username').type('renatatest1');
-    cy.get('name=password').type('password');
+  cy.get('[name=username]').type('renatatest1');
+  cy.get('[name=password]').type('password');
 
-    // Click submit button
-    cy.contains('button[type=submit]').click();
-  })
+  cy.get('button[type=submit]').click();
 
-})
+  cy.contains('.v-btn', 'New').should('exist');
+
+  // Create a new Report
+  cy.wait(1000);
+  cy.contains('.v-btn', 'New').click();
+
+  cy.contains('.v-dialog', /Compose a New Report .*/).as('report-dialog');
+  cy.get('@report-dialog').should('exist');
+
+  // Select the 'Qa Question' Report type
+  cy.get('@report-dialog').find('input[name=channel]').first().click();
+  cy.contains('.v-list-item', 'Qa Questions').click();
+
+  cy.contains('button[type=submit]', 'Compose Report')
+    .should('not.have.class', 'v-btn--disabled')
+    .click();
+
+  // Complete the report, filling in all answers
+  cy.contains('.report-form__step__element', 'Country?').find('input').click();
+  cy.contains('.v-list-item', 'Albania').click();
+
+  cy.contains('.report-form__step__element', 'How this happened?')
+    .find('input')
+    .type('Test answer');
+
+  cy.contains('.report-form__step__element', 'Give more details')
+    .find('textarea')
+    .type('Test details');
+
+  cy.contains('.report-form__step__element', 'Select multiple').as(
+    'select-multiple'
+  );
+  cy.get('@select-multiple').contains('.v-input__control', 'Option 1').click();
+  cy.get('@select-multiple').contains('.v-input__control', 'Option 3').click();
+
+  cy.contains('.report-form__step__element', 'Inform number')
+    .find('input')
+    .type('Test number');
+
+  cy.contains('.report-form__step__element', 'Date?').find('input').click();
+  cy.get('.v-date-picker-table').contains('.v-btn', '12');
+  cy.get('.v-date-picker-table').contains('.v-btn', 'Ok');
+
+  cy.contains('.report-form__step__element', 'Who?').find('input').click();
+  cy.contains('.v-list-item', 'Qa Test').click();
+
+  cy.contains('.v-btn', 'Save Progress and Continue').click();
+
+  cy.contains('.report-form__step__element', 'Multiple Choice Radiobutton')
+    .contains('.v-radio', 'Option B')
+    .click();
+
+  cy.contains(
+    '.report-form__step__element',
+    'Multiple Choice Single Selection Dropdown'
+  )
+    .find('v-input__slot')
+    .click();
+  cy.contains('.v-list-item__title', 'Option II').click();
+
+  cy.contains(
+    '.report-form__step__element',
+    'Multiple Choice Checkbox Dropdown'
+  )
+    .find('v-input__slot')
+    .click();
+  cy.contains('.v-list-item__title', 'Option One').click();
+  cy.contains('.v-list-item__title', 'Option Two').click();
+
+  cy.contains('.v-btn', 'Save Progress and Continue').click();
+
+  // Submit the report
+  cy.contains('.v-btn', 'Submit Report').click();
+  cy.contains('.v-dialog', /Your report has been sent .*/);
+});
